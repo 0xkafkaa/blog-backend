b2e53ca80c49d77f57a5e040be268575c7c75bac
mklno <manodreuel@gmail.com>
2024-09-17 08:52:22 +0530

    user model added

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [/^\S+@\S+\.\S+$/, "Please add a valid email"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "author", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function () {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+UserSchema.methods.getSignedJwt = function () {
+  return jwt.sign({ user: this }, "123456", { expiresIn: "30d" });
+};
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "hello world" });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
