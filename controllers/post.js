81ec80ae3f107d3a9b7ee7166cca2c56279de619
mklno <manoprasanth.mathavan@gmail.com>
2024-09-19 18:09:38 +0530

    added update and delete post func

--- controllers/post.js
+++ controllers/post.js
@@ -1,16 +1,17 @@
 const Post = require("../models/Post");
+const { tryCatch } = require("../utils/tryCatch");
 
-exports.getPosts = async function (req, res) {
+exports.getPosts = tryCatch(async function (req, res) {
   const posts = await Post.find();
   res.status(200).json({ posts });
-};
+});
 
-exports.getPost = async function (req, res) {
+exports.getPost = tryCatch(async function (req, res) {
   const post = await Post.findById(req.params.id);
   res.status(200).json({ post });
-};
+});
 
-exports.createPost = async function (req, res) {
+exports.createPost = tryCatch(async function (req, res) {
   const user = req.user._id;
   const { postTitle, postBody, genre } = req.body;
   const post = await Post.create({ postTitle, postBody, genre, user });
@@ -19,4 +20,47 @@ exports.createPost = async function (req, res) {
     success: true,
     data: post,
   });
-};
+});
+
+exports.updatePost = tryCatch(async function (req, res, next) {
+  let post = await Post.findById(req.params.id);
+  if (!post) {
+    return next({
+      message: "Post not found",
+    });
+  }
+  if (post.user.toString() !== req.user._id) {
+    return next({
+      message: "Not authorized to update this post",
+    });
+  }
+  post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: post,
+  });
+});
+
+exports.deletePost = tryCatch(async function (req, res, next) {
+  let post = await Post.findById(req.params.id);
+  if (!post) {
+    return next({
+      message: "Post not found",
+    });
+  }
+  if (post.user.toString() !== req.user._id) {
+    return next({
+      message: "Not authorized to delete this post",
+    });
+  }
+
+  await Post.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});
