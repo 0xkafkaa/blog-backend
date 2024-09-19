68fb45d2c29ea003a7510a96b01725cf81e00f28
mklno <manodreuel@gmail.com>
2024-09-19 12:19:58 +0530

    server updated

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const ErrorResponse = require("./middleware/ErrorResponse");
 
 app.use(express.json());
 const user = require("./routes/user");
+const post = require("./routes/post");
 const connectDB = require("./db/index");
 connectDB();
 
@@ -12,6 +14,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", user);
+app.use("/posts", post);
+
+app.use(ErrorResponse);
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`)
