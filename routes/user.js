bcfbbd1ead9266bd46ce0d7252a9c9a241ad1009
mklno <manoprasanth.mathavan@gmail.com>
2024-09-20 11:03:35 +0530

    import and updates

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { register, login } = require("../controllers/user");
-
+const { register, login, getAllUsers } = require("../controllers/user");
+const { authenticate, authorize } = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/", authenticate, authorize(["admin"]), getAllUsers);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const ErrorResponse = require("./middleware/ErrorResponse");
 app.use(express.json());
 const user = require("./routes/user");
 const post = require("./routes/post");
+const comment = require("./routes/comment");
 const connectDB = require("./db/index");
 connectDB();
 
@@ -15,9 +16,8 @@ app.get("/", (req, res) => {
 
 app.use("/user", user);
 app.use("/posts", post);
-
+app.use("/comments", comment);
 app.use(ErrorResponse);
-
 
 app.listen(process.env.PORT, () =>
   console.log(`Listening on port ${process.env.PORT}`)
